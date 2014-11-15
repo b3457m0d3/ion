dfccc22e4a58dee9e455ec6af25d44da6184c5f0
Jessie Taylor <b3457m0d3@interr0bang.net>
2014-11-15 14:27:31 -0500

    fixed association example

---
+++ root/js/ion/Helper/Index.js
@@ -0,0 +1,29 @@
+define(["jquery","underscore","backbone","IndexModel","IndexView"],
+  function($,_,Backbone,IndexModel,IndexView){
+    var Flag = Backbone.Model.extend({ /* ... */ }),
+        City = Backbone.Model.extend({ /* ... */ }),
+        Cities = Backbone.Collection.extend({ model: City });
+    Backbone.associate(IndexModel,{
+        flag: { type: Flag }, //Flag is a model (hasOne)
+        cities: { type: Cities } // Cities is a collection (hasMany)
+    });
+	var Index = {};
+    Index.Model  = new IndexModel({
+        name: 'index',
+        title: 'Home',
+        el: '.viewPort',
+        flag: {
+            colors: ['red','white']
+        },
+        cities: [
+            { name: 'Calgary' },
+            { name: 'Regina' }
+        ]
+	});
+    _.each(Index.Model.cities().at(1),function(city){
+        if(_.has(city,'name')) _.log(city.name);
+    });
+    Index.View = new IndexView.Layout({model:Index.Model});
+    Index.View.model.set({layout:Index.View});
+	return Index;
+});
